95059a774adea6afde71f99cb04c6a3b52450b81
Tanise Santos <tanise234@gmail.com>
2023-02-16 19:22:33 -0300

    :sparkles:feat: is_palindrome_recursive() created

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+
+    if len(word) <= 3 or high_index - low_index <= 1:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
