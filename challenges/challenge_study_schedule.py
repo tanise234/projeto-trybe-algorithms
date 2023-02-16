376c799b565540dce519c9a8d6b0d4113a27e8be
Tanise Santos <tanise234@gmail.com>
2023-02-16 16:52:42 -0300

    :sparkles:feat: study_schedule() implemented

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period, target_time=None):
+    try:
+        valid(target_time)
+
+        counter = 0
+        for period in permanence_period:
+            valid(period[0])
+            valid(period[1])
+
+            if period[0] <= target_time <= period[1]:
+                counter += 1
+        return counter
+
+    except TypeError:
+        return None
+
+
+def valid(hour):
+    if not (type(hour) == int and 0 <= hour < 24):
+        raise TypeError
