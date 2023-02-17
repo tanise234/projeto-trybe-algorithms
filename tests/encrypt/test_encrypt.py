e885e636c3bdb39315c3602d39ba7c4ce49fe1f7
Tanise Santos <tanise234@gmail.com>
2023-02-17 00:05:40 -0300

    🧪test: encrypt_message()

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,24 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+VALID_MESSAGE = "valid"
+INVALID_MESSAGE = 5
+
+VALID_KEY = 2
+INVALID_KEY = "invalid"
+
+OOD_INDEX = 3
+EVEN_INDEX = 4
+OUT_OF_RANGE_INDEX = 9
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message(VALID_MESSAGE, 3) == "lav_di"
+    assert encrypt_message(VALID_MESSAGE, 4) == "d_ilav"
+    assert encrypt_message(VALID_MESSAGE, 9) == "dilav"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(VALID_MESSAGE, INVALID_KEY)
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(INVALID_MESSAGE, VALID_KEY)
