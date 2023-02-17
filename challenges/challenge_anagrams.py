644c6b3ed4b50cacec3ba440f5846509be49563f
Tanise Santos <tanise234@gmail.com>
2023-02-16 23:37:15 -0300

    :sparkles:feat: is_anagram() implemented

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,45 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first = "".join(merge_sort(list(first_string.lower())))
+    second = "".join(merge_sort(list(second_string.lower())))
+    result = first == second
+
+    if first == "" or second == "":
+        result = False
+
+    return (first, second, result)
+
+
+# THE CODE BELOW IS FROM TRYBE COURSE
+# https://app.betrybe.com/learn/course/
+
+
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)  # dividindo as listas
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end)  # unindo as listas
+    return numbers
+
+
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid]  # indexando a lista da esquerda
+    right = numbers[mid:end]  # indexando a lista da direita
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
