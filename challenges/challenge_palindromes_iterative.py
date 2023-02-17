ad419ca4985e4173c37a40b6e80b932e7f64e582
Tanise Santos <tanise234@gmail.com>
2023-02-17 00:24:20 -0300

    :spakles:feat: is_palindrome_iterative() created

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    mid_index = len(word) // 2
+    for index in range(mid_index):
+        if word[index] != word[-(index + 1)]:
+            return False
+    return True
