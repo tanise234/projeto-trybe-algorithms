30fbf0a813a42dd30fb9754bbc27c0e7f2664a2d
Tanise Santos <tanise234@gmail.com>
2023-02-17 13:14:19 -0300

    🔨update: find_duplicate()

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,18 +2,22 @@ from collections import Counter
 
 
 def find_duplicate(nums):
-    if len(nums) >= 2 and valid(nums):
-        repeated = Counter(nums).most_common()
+    if not is_valid(nums):
+        return False
 
-        if repeated[1][1] and repeated[1][1] > 1:
-            return False
+    repeated = Counter(nums).most_common()
+    if (len(repeated) > 1 and repeated[1][1] > 1) or repeated[0][1] == 1:
+        return False
 
-        return repeated[0][0]
-
-    return False
+    return repeated[0][0]
 
 
-def valid(nums):
+def is_valid(nums):
+    if len(nums) < 2:
+        return False
+
     for num in nums:
-        if type(num) != int or num <= 1:
+        if type(num) != int or num < 1:
             return False
+
+    return True
