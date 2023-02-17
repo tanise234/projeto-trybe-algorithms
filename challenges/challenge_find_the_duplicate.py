d2f015cff14d54ad738b56a478d2c4247be3a6f5
Tanise Santos <tanise234@gmail.com>
2023-02-17 02:00:34 -0300

    ✨feat: find_duplicate created, needs Big-o imrpove

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
+from collections import Counter
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) >= 2 and valid(nums):
+        repeated = Counter(nums).most_common()
+        
+        if repeated[1][1] and repeated[1][1] > 1:
+            return False
+
+        return repeated[0][0]
+
+    return False
+
+
+def valid(nums):
+    for num in nums:
+        if type(num) != int or num <= 1:
+            return False
